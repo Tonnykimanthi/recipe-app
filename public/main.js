7b3a4148b4dd1bddfaae6dac84058c37b534e96b
Tony Km <kimaxhanthony9@gmail.com>
2023-08-14 20:00:08 +0300

    Apis

--- public/main.js
+++ public/main.js
@@ -32,9 +32,9 @@ const randomMeal = async () => {
     let meal = document.createElement('li');
 
     meal.innerHTML = `
-    <li class="recipe border w-80 h-64 rounded-xl overflow-hidden relative">
+    <li class="recipe border w-96 h-64 rounded-xl overflow-hidden relative">
     <img class="object-cover" src="${mealImg}" alt="${mealName}">
-    <div class="flex justify-between items-center absolute bottom-0 left-0 right-0 py-1.5 px-4 bg-white">
+    <div class="flex justify-between items-center absolute bottom-0 left-0 right-0 py-1.5 px-4 bg-white/80">
         <span class="recipe-name text-black text-lg font-medium">${mealName}</span>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 cursor-pointer hover:fill-gray-100">
             <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
