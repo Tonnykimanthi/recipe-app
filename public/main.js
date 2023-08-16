bfca500db24c2cbc6dabcb65d8e3b863316f5e2c
Tony Km <kimaxhanthony9@gmail.com>
2023-08-16 18:35:33 +0300

    Search meal by name

--- public/main.js
+++ public/main.js
@@ -67,33 +67,49 @@ const displayMealsByCategories = async () => {
   const mealsArray = await mealsByCategories();
 
   for (const meal of mealsArray) {
-    displayMeals(meal);
+   displayMeals(meal);
   }
 };
 
 
-const searchMealByName = async () => {
-  const fetchMeals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
+const searchMealByName = async (name) => {
+  const fetchMeals = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`)
   const mealsJson = await fetchMeals.json()
   console.log(mealsJson)
   const mealArray = mealsJson.meals;
 
+  const mealEl = document.createElement('li')
+
   if(mealArray){
     recipeContainer.innerHTML = '';
+
     for(const meal of mealArray){
-      displayMeals(meal)
+      mealEl.innerHTML = `
+      <li class="recipe border w-[23rem] h-64 rounded-xl overflow-hidden relative">
+      <img class="object-cover w-full" src="${meal.strMealThumb}" alt="${meal.strMeal}">
+      <div class="flex justify-between items-center absolute bottom-0 left-0 right-0 py-1.5 px-4 bg-gray-100/60">
+          <span class="recipe-name text-black text-lg font-medium">${meal.strMeal}</span>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.5" stroke="currentColor" class="star-btn w-6 h-6 cursor-pointer">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+            </svg>   
+      </div>
+  </li>
+      `
+      recipeContainer.append(mealEl)
     }
   }else{
-    recipeContainer.innerHTML = "<p>Not Found</p>"
+    recipeContainer.innerHTML = "<p>Not found</p>"
   }
 }
+
 const searchBtn = document.getElementById('search-btn');
-searchBtn.addEventListener('click', ()=>{
-  const searchInput = document.getElementById('search-input').value.trim();
+searchBtn.addEventListener('click',  ()=>{
+  const searchInput = document.getElementById('search-input').value;
 
   if(searchInput !== ''){
-   searchMealByName()
+   
+    searchMealByName(searchInput)
   }
 })
-
+// searchMealByName()
 displayMealsByCategories();
