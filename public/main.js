ead2b4567425ff48146b8da08efeeeb648b95e2d
Tony Km <kimaxhanthony9@gmail.com>
2023-08-17 22:33:06 +0300

    Local storage

--- public/main.js
+++ public/main.js
@@ -18,6 +18,7 @@ closeMenuBtn.addEventListener("click", () => {
 const recipeContainer = document.querySelector(".recipe-c");
 const recipeImg = document.querySelector(".recipe img");
 const recipeName = document.querySelector(".recipe-name");
+const favContainer = document.getElementById("favourites-container");
 
 // Fetch meal by category
 const mealsByCategories = async () => {
@@ -125,23 +126,6 @@ searchBtn.addEventListener("click", () => {
   }
 });
 
-// Display favourite meals
-
-const displayFavMeals = () => {
-  const mealEl = document.createElement("li");
-  mealEl.innerHTML = `
-  <li class="recipe border w-[23rem] h-64 rounded-xl overflow-hidden relative">
-  <img class="object-cover w-full" src="https://www.themealdb.com/images/category/breakfast.png" alt="breakfast" loading="lazy">
-  <div class="flex justify-between items-center absolute bottom-0 left-0 right-0 py-1.5 px-4 bg-gray-100/60">
-      <span class="recipe-name text-black text-lg font-medium">Breakfast</span>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.5" stroke="currentColor" class="star-btn w-6 h-6 cursor-pointer">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-        </svg>   
-  </div>
-</li>
-  `;
-};
-
 // Add to LS
 const addMealToLS = (mealId) => {
   const getMeals = getMealFromLS() || [];
@@ -166,3 +150,42 @@ const removeMealFromLS = (mealId) => {
 
   localStorage.setItem("mealId", JSON.stringify(existingInLS));
 };
+
+const fetchMealById = async (id) => {
+  const resp = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i= ${id}`
+  );
+  const respJson = await resp.json();
+  console.log(respJson);
+};
+
+// Display favourite meals
+const displayFavMeals = async () => {
+  const getFromLS = getMealFromLS() || [];
+
+  for (mealId of getFromLS) {
+    const meal = await fetchMealById(mealId);
+
+    if (meal) {
+      const mealEl = document.createElement("li");
+
+      mealEl.innerHTML = `
+  <li class="recipe border w-[23rem] h-64 rounded-xl overflow-hidden relative">
+  <img class="object-cover w-full" src="${meal.strCategoryThumb}" alt="${meal.strCategory}" loading="lazy">
+  <div class="flex justify-between items-center absolute bottom-0 left-0 right-0 py-1.5 px-4 bg-gray-100/60">
+      <span class="recipe-name text-black text-lg font-medium">${meal.strCategory}</span>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.5" stroke="currentColor" class="star-btn w-6 h-6 cursor-pointer">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+        </svg>   
+  </div>
+</li>
+  `;
+
+      favContainer.append(mealEl);
+    }
+  }
+};
+
+displayFavMeals();
+
+fetchMealById();
