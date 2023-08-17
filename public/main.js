e393e256a6ea047e13ebd92e8d9822321dff4ddd
Tony Km <kimaxhanthony9@gmail.com>
2023-08-17 21:48:01 +0300

    LS for favourite meals

--- public/main.js
+++ public/main.js
@@ -34,8 +34,9 @@ const mealsByCategories = async () => {
 const displayMeals = (categories) => {
   const mealImg = categories.strCategoryThumb;
   const mealName = categories.strCategory;
+  const mealId = categories.idCategory;
 
-  let mealEl = document.createElement("ul");
+  let mealEl = document.createElement("li");
 
   mealEl.innerHTML = `
   <li class="recipe border w-[23rem] h-64 rounded-xl overflow-hidden relative">
@@ -50,42 +51,43 @@ const displayMeals = (categories) => {
     `;
   recipeContainer.append(mealEl);
 
-  const btn = mealEl.querySelector('.recipe div .star-btn')
+  const btn = mealEl.querySelector(".recipe div .star-btn");
 
-  btn.addEventListener('click', () => {
-    if(btn.classList.contains('fill-orange-500')){
-      btn.classList.remove('fill-orange-500')
-    }else{
-      btn.classList.add('fill-orange-500')
+  btn.addEventListener("click", () => {
+    if (btn.classList.contains("fill-orange-500")) {
+      btn.classList.remove("fill-orange-500");
+      removeMealFromLS(mealId);
+    } else {
+      btn.classList.add("fill-orange-500");
+      addMealToLS(mealId);
     }
-    
-  })
-
+  });
 };
 
 const displayMealsByCategories = async () => {
   const mealsArray = await mealsByCategories();
 
   for (const meal of mealsArray) {
-   displayMeals(meal);
+    displayMeals(meal);
   }
 };
 
 displayMealsByCategories();
 
-
 // Search meal by name
 const searchMealByName = async (name) => {
-  const fetchMeals = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`)
-  const mealsJson = await fetchMeals.json()
-  console.log(mealsJson)
+  const fetchMeals = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`
+  );
+  const mealsJson = await fetchMeals.json();
+  console.log(mealsJson);
   const mealArray = mealsJson.meals;
 
-  if(mealArray){
-    recipeContainer.innerHTML = '';
+  if (mealArray) {
+    recipeContainer.innerHTML = "";
 
-    for(const meal of mealArray){
-      const mealEl = document.createElement('ul')
+    for (const meal of mealArray) {
+      const mealEl = document.createElement("li");
       mealEl.innerHTML = `
       <li class="recipe border w-[23rem] h-64 rounded-xl overflow-hidden relative">
       <img class="object-cover w-full" src="${meal.strMealThumb}" alt="${meal.strMeal}" loading="lazy">
@@ -96,36 +98,71 @@ const searchMealByName = async (name) => {
             </svg>   
       </div>
   </li>
-      `
-      recipeContainer.append(mealEl)
+      `;
+      recipeContainer.append(mealEl);
 
-      const btn = mealEl.querySelector('.recipe div .star-btn')
+      const btn = mealEl.querySelector(".recipe div .star-btn");
 
-      btn.addEventListener('click', () => {
-        if(btn.classList.contains('fill-orange-500')){
-          btn.classList.remove('fill-orange-500')
-        }else{
-          btn.classList.add('fill-orange-500')
+      btn.addEventListener("click", () => {
+        if (btn.classList.contains("fill-orange-500")) {
+          btn.classList.remove("fill-orange-500");
+        } else {
+          btn.classList.add("fill-orange-500");
         }
-        
-      })
+      });
     }
-  }else{
-    recipeContainer.innerHTML = "<p>Not found</p>"
+  } else {
+    recipeContainer.innerHTML = "<p>Not found</p>";
   }
+};
 
+const searchBtn = document.getElementById("search-btn");
+searchBtn.addEventListener("click", () => {
+  const searchInput = document.getElementById("search-input").value.trim();
 
-
-}
-
-const searchBtn = document.getElementById('search-btn');
-searchBtn.addEventListener('click',  ()=>{
-  const searchInput = document.getElementById('search-input').value.trim();
-
-  if(searchInput !== ''){
-   
-    searchMealByName(searchInput)
+  if (searchInput !== "") {
+    searchMealByName(searchInput);
   }
-})
+});
 
+// Display favourite meals
 
+const displayFavMeals = () => {
+  const mealEl = document.createElement("li");
+  mealEl.innerHTML = `
+  <li class="recipe border w-[23rem] h-64 rounded-xl overflow-hidden relative">
+  <img class="object-cover w-full" src="https://www.themealdb.com/images/category/breakfast.png" alt="breakfast" loading="lazy">
+  <div class="flex justify-between items-center absolute bottom-0 left-0 right-0 py-1.5 px-4 bg-gray-100/60">
+      <span class="recipe-name text-black text-lg font-medium">Breakfast</span>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.5" stroke="currentColor" class="star-btn w-6 h-6 cursor-pointer">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+        </svg>   
+  </div>
+</li>
+  `;
+};
+
+// Add to LS
+const addMealToLS = (mealId) => {
+  const getMeals = getMealFromLS() || [];
+
+  const existingInLS = getMeals.find((meal) => meal.mealId === mealId);
+
+  if (!existingInLS) {
+    getMeals.push(mealId);
+    localStorage.setItem("mealId", JSON.stringify(getMeals));
+  }
+};
+
+// Get from LS
+const getMealFromLS = () => {
+  JSON.parse(localStorage.getItem("mealId"));
+};
+
+// Remove from LS
+const removeMealFromLS = (mealId) => {
+  const getMeals = getMealFromLS() || [];
+  const existingInLS = getMeals.filter((meal) => meal.mealId !== mealId);
+
+  localStorage.setItem("mealId", JSON.stringify(existingInLS));
+};
