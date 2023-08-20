c7aa9b1c5626d6e506fa4ea91ec453ff37094976
Tony Km <kimaxhanthony9@gmail.com>
2023-08-21 00:32:03 +0300

    Fav meals LS

--- public/main.js
+++ public/main.js
@@ -186,15 +186,29 @@ const displayFavMeals = async () => {
   <img class="object-cover w-full" src="${meal.strMealThumb}" alt="${meal.strMeal}" loading="lazy">
   <div class="flex justify-between items-center absolute bottom-0 left-0 right-0 py-1.5 px-4 bg-gray-100/60">
       <span class="recipe-name text-black text-lg font-medium">${meal.strMeal}</span>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.5" stroke="currentColor" class="star-btn w-6 h-6 cursor-pointer">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.5" stroke="currentColor" class="star-btn w-6 h-6 fill-orange-500 cursor-pointer">
           <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
         </svg>   
   </div>
 </li>
   `;
       favContainer.append(mealEl);
+      
+      // *****UNFINISHED
+      const favBtn = mealEl.querySelector(".recipe div .star-btn")
+      favBtn.addEventListener("click", ()=>{
+        if(favBtn.classList.contains("fill-orange-500")){
+          favBtn.classList.remove("fill-orange-500");
+          removeMealFromLS(meal.idMeal);
+        }else{
+          favBtn.classList.add("fill-orange-500");
+          // addMealToLS(meal.idMeal)
+        }
+        fetchMealById(meal.idMeal)
+      })
     }
   }
+  
 };
 
 displayFavMeals();
