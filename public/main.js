c3b97f923eab9c5c7e2a1ad51f59a45e7ea99d81
Tony Km <kimaxhanthony9@gmail.com>
2023-08-21 17:49:31 +0300

    Responsiveness

--- public/main.js
+++ public/main.js
@@ -2,6 +2,17 @@ const openMenuBtn = document.querySelector(".open-menu-btn");
 const closeMenuBtn = document.querySelector(".close-menu-btn");
 const menuForSm = document.querySelector(".nav-for-sm");
 
+const homeBody = document.querySelector(".home-body");
+const recipeBody = document.getElementById("recipe-body");
+const favBody = document.getElementById("fav-body");
+const homeBtn = document.querySelectorAll(".home-btn");
+const recipeBtn = document.querySelectorAll(".recipe-btn");
+const favBtn = document.querySelectorAll(".fav-btn");
+const recipeContainer = document.querySelector(".recipe-c");
+const favContainer = document.getElementById("favourites-container");
+
+
+// Nav/Menus
 openMenuBtn.addEventListener("click", () => {
   openMenuBtn.classList.remove("max-sm:block");
   closeMenuBtn.classList.remove("hidden");
@@ -15,21 +26,40 @@ closeMenuBtn.addEventListener("click", () => {
 });
 
 // RECIPE PAGE
-const recipeContainer = document.querySelector(".recipe-c");
-const recipeImg = document.querySelector(".recipe img");
-const recipeName = document.querySelector(".recipe-name");
-const favContainer = document.getElementById("favourites-container");
+// const recipeImg = document.querySelector(".recipe img");
+// const recipeName = document.querySelector(".recipe-name");
 
-const favBody = document.getElementById("fav-body");
-const openFavBtn = document.getElementById("open-fav-btn");
-const closeFavBtn = document.getElementById("close-fav-btn");
+// Nav event listeners
+homeBtn.forEach((btn)=>{
+  btn.addEventListener("click", ()=>{
+    if(homeBody.classList.contains("hidden")){
+      homeBody.classList.remove("hidden");
+      recipeBody.classList.add("hidden");
+      favBody.classList.add("hidden");
+    }
+  })
+});
+recipeBtn.forEach((btn)=>{
+  btn.addEventListener("click", ()=>{
+    if(recipeBody.classList.contains("hidden")){
+      recipeBody.classList.remove("hidden");
+      favBody.classList.add("hidden");
+      homeBody.classList.add("hidden");
+    }
+  })
+});
 
-openFavBtn.addEventListener("click", () => {
-  favBody.classList.remove("hidden")
+favBtn.forEach((btn)=>{
+  btn.addEventListener("click", ()=>{
+    if(favBody.classList.contains("hidden")){
+      favBody.classList.remove("hidden");
+      recipeBody.classList.add("hidden");
+      homeBody.classList.add("hidden");
+    }
+  })
 });
-closeFavBtn.addEventListener("click", () => {
-  favBody.classList.add("hidden")
-});
+
+
 
 // Fetch meal by category
 const mealsByCategories = async () => {
@@ -50,8 +80,8 @@ const displayMeals = (categories) => {
   let mealEl = document.createElement("li");
 
   mealEl.innerHTML = `
-  <li class="recipe border w-[23rem] h-64 rounded-xl overflow-hidden relative">
-    <img class="object-cover w-full" src="${mealImg}" alt="${mealName}" loading="lazy">
+  <li class="recipe border border-slate-100 h-64 rounded-xl overflow-hidden cursor-pointer relative">
+    <img class="object-cover w-full h-full" src="${mealImg}" alt="${mealName}" loading="lazy">
     <div class="flex justify-between items-center absolute bottom-0 left-0 right-0 py-1.5 px-4 bg-gray-100/60">
         <span class="recipe-name text-black text-lg font-medium">${mealName}</span>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.5" stroke="currentColor" class="star-btn w-6 h-6 cursor-pointer">
@@ -77,7 +107,7 @@ const displayMealsByCategories = async () => {
   const mealsArray = await mealsByCategories();
 
   for (const meal of mealsArray) {
-    displayMeals(meal);
+   displayMeals(meal);
   }
 };
 
@@ -96,10 +126,10 @@ const searchMealByName = async (name) => {
     recipeContainer.innerHTML = "";
 
     for (const meal of mealArray) {
-      const mealEl = document.createElement("li");
+      const mealEl = document.createElement("div");
       mealEl.innerHTML = `
-      <li class="recipe border w-[23rem] h-64 rounded-xl overflow-hidden relative">
-      <img class="object-cover w-full" src="${meal.strMealThumb}" alt="${meal.strMeal}" loading="lazy">
+      <li class="recipe border w-full h-64 rounded-xl overflow-hidden relative cursor-pointer">
+        <img class="object-cover w-full" src="${meal.strMealThumb}" alt="${meal.strMeal}" loading="lazy">
       <div class="flex justify-between items-center absolute bottom-0 left-0 right-0 py-1.5 px-4 bg-gray-100/60">
           <span class="recipe-name text-black text-lg font-medium">${meal.strMeal}</span>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.5" stroke="currentColor" class="star-btn w-6 h-6 cursor-pointer">
@@ -179,12 +209,12 @@ const displayFavMeals = async () => {
   for (const mealId of getFromLS) {
     const meal = await fetchMealById(mealId);
     if (meal) {
-      const mealEl = document.createElement("li");
+      const mealEl = document.createElement("div");
 
       mealEl.innerHTML = `
-  <li class="recipe border w-[23rem] h-64 rounded-xl overflow-hidden relative">
-  <img class="object-cover w-full" src="${meal.strMealThumb}" alt="${meal.strMeal}" loading="lazy">
-  <div class="flex justify-between items-center absolute bottom-0 left-0 right-0 py-1.5 px-4 bg-gray-100/60">
+  <li class="recipe h-64 rounded-xl overflow-hidden relative cursor-pointer">
+    <img class="object-cover w-full" src="${meal.strMealThumb}" alt="${meal.strMeal}" loading="lazy">
+    <div class="flex justify-between items-center absolute bottom-0 left-0 right-0 py-1.5 px-4 bg-gray-100/60">
       <span class="recipe-name text-black text-lg font-medium">${meal.strMeal}</span>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.5" stroke="currentColor" class="star-btn w-6 h-6 fill-orange-500 cursor-pointer">
           <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
@@ -202,7 +232,7 @@ const displayFavMeals = async () => {
           removeMealFromLS(meal.idMeal);
         }else{
           favBtn.classList.add("fill-orange-500");
-          // addMealToLS(meal.idMeal)
+          addMealToLS(meal.idMeal)
         }
         fetchMealById(meal.idMeal)
       })
