d1f589ae342738319e0f4770e913ce4b57e66be8
Tony Km <kimaxhanthony9@gmail.com>
2023-10-09 16:19:25 +0300

    Display meal descriptions for the stored/fav meals

--- public/main.js
+++ public/main.js
@@ -250,10 +250,17 @@ const displayFavMeals = async () => {
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.5" stroke="currentColor" class="star-btn w-6 h-6 fill-orange-500 cursor-pointer">
           <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
         </svg>   
-  </div>
-</li>
+    </div>
+  </li>
   `;
       favContainer.append(mealEl);
+      showDescription(
+        mealEl,
+        meal.strMeal,
+        meal.strMealThumb,
+        meal.strInstructions,
+        meal
+      );
       const favBtn = mealEl.querySelector(".recipe div .star-btn");
       favBtn.addEventListener("click", () => {
         favContainer.innerHTML = "";
@@ -278,19 +285,22 @@ const showDescription = (mealEl, mealName, mealImg, instructions, meal) => {
 
   mealEl.addEventListener("click", () => {
     descriptionContainer.classList.remove("hidden");
+    descriptionContainer.classList.add("flex");
 
     const ingredients = [];
     console.log(ingredients);
 
     for (let i = 1; i <= 20; i++) {
       if (meal["strIngredient" + i]) {
-        ingredients.push(`${meal["strIngredient"+i]} - ${meal["strMeasure" +i]}`);
+        ingredients.push(
+          `${meal["strIngredient" + i]} - ${meal["strMeasure" + i]}`
+        );
       }
     }
 
     const sectionEl = document.createElement("section");
     sectionEl.className =
-      "mt-16 pt-10 px-4 py-4 w-full max-w-md flex flex-col items-center rounded-lg bg-white relative overflow-y-auto";
+      "mt-16 pt-10 px-4 py-4 w-full max-w-lg flex flex-col items-center rounded-lg bg-white relative overflow-y-auto";
 
     sectionEl.innerHTML = `
     <button class="close-btn absolute top-3 right-3 hover:text-red-400">
@@ -302,7 +312,7 @@ const showDescription = (mealEl, mealName, mealImg, instructions, meal) => {
         <img class="mt-6" src="${mealImg}" alt="${mealName}">
         <h4 class="mt-5 font-medium text-xl">Ingredients</h4>
         <ul class="list-disc">
-            ${ingredients.map((ingre) => `<li>${ingre}</li>`).join('')}
+            ${ingredients.map((ingre) => `<li>${ingre}</li>`).join("")}
         </ul>
         <h5 class="mt-4 font-medium text-xl">Instructions</h5>
         <p>${instructions}</p>
